3bcde60ad4d37f7cb8c32db505d36abd4e5bd46c
Tommycore <lyric_me@web.de>
2025-08-26 20:02:25 +0200

    Add custom tile entity

---
+++ TCAdvancedLockPicking/TileEntities/TileEntityPickableDoor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileEntities
+{
+    public class TileEntityPickableDoor : TileEntitySecureDoor
+    {
+        private int requiredLockPickTier = 0;
+
+        public int RequiredLockPickTier
+        {
+            get => requiredLockPickTier;
+            set => requiredLockPickTier = value;
+        }
+
+        public TileEntityPickableDoor(Chunk _chunk) : base(_chunk)
+        {
+        }
+    }
+}
