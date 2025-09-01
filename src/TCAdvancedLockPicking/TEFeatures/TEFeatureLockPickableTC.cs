6d688b1f7461616f8e9920f3ffa031c16e75104e
Tommycore <lyric_me@web.de>
2025-09-01 15:09:02 +0200

    Add custom LockPickable TEF

---
+++ src/TCAdvancedLockPicking/TEFeatures/TEFeatureLockPickableTC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Platform;
+
+namespace TEFeatures
+{
+    public class TEFeatureLockPickableTC : TEFeatureLockPickable
+    {
+        public override void UpdateBlockActivationCommands(ref BlockActivationCommand _command, ReadOnlySpan<char> _commandName, WorldBase _world, Vector3i _blockPos, BlockValue _blockValue, EntityAlive _entityFocusing)
+        {
+            base.UpdateBlockActivationCommands(ref _command, _commandName, _world, _blockPos, _blockValue, _entityFocusing);
+            if (base.CommandIs(_commandName, "pick"))
+            {
+                Log.Out("[TC-ALP] TEFeatureLockPickableTC.UpdateBlockActivationCommands");
+                Log.Out($"[TC-ALP] Locked: {lockFeature.IsLocked()}");
+                Log.Out($"[TC-ALP] IsUserAllowed: {lockFeature.IsUserAllowed(PlatformManager.InternalLocalUserIdentifier)}");
+                _command.enabled = (lockFeature.IsLocked() && !lockFeature.IsUserAllowed(PlatformManager.InternalLocalUserIdentifier));
+                Log.Out($"[TC-ALP] Pick command enabled: {_command.enabled}");
+
+                return;
+            }
+        }
+    }
+}
