dfb4bc39c94cc361231eab91e0ceff1b7d1b57da
Tommycore <lyric_me@web.de>
2025-09-04 21:47:17 +0200

    Comment out unusable stuff

--- src/0-DevTools/HarmonyPatches/GameStarter.cs
+++ src/0-DevTools/HarmonyPatches/GameStarter.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DevStartMenu;
+using DevTools.DevStartMenu;
 using HarmonyLib;
 
-namespace HarmonyPatches
+namespace DevTools.HarmonyPatches
 {
     public class GameStarter
     {
@@ -14,34 +11,33 @@ namespace HarmonyPatches
             Environment.GetCommandLineArgs()
                 .Any(argument => argument.EqualsCaseInsensitive("-devstart"));
 
-        [HarmonyPatch(typeof(XUiC_MainMenu), nameof(XUiC_MainMenu.Open))]
-        public class XUiC_MainMenu_Open
-        {
-            private static bool Prefix(XUiC_MainMenu __instance, XUi _xuiInstance)
-            {
-                Log.Out("[TC-DT] XUiC_MainMenu_Open");
-                return true;
+        //[HarmonyPatch(typeof(XUiC_MainMenu), nameof(XUiC_MainMenu.Open))]
+        //public class XUiC_MainMenu_Open
+        //{
+        //    private static bool Prefix(XUiC_MainMenu __instance, XUi _xuiInstance)
+        //    {
+        //        Log.Out("[TC-DT] XUiC_MainMenu_Open");
 
-                if (HasDevStartLaunchArgument)
-                {
-                    // Skip straight to opening the main menu
-                    //_xuiInstance.playerUI.windowManager.Open(XUiC_DevStartMainMenu.ID, true, false, true);
-                    _xuiInstance.playerUI.windowManager.Open(XUiC_MainMenu.ID, true, false, true);
-                    return false;
-                }
+        //        if (HasDevStartLaunchArgument)
+        //        {
+        //            // Skip straight to opening the main menu
+        //            _xuiInstance.playerUI.windowManager.Open(XUiC_DevStartMainMenu.ID, true, false, true);
+        //            //_xuiInstance.playerUI.windowManager.Open(XUiC_MainMenu.ID, true, false, true);
+        //            return false;
+        //        }
 
-                return true;
-            }
-        }
+        //        return true;
+        //    }
+        //}
 
-        [HarmonyPatch(typeof(XUiC_MainMenu), nameof(XUiC_MainMenu.OnOpen))]
-        public class XUiC_MainMenu_OnOpen
-        {
-            private static void Postfix(XUiC_MainMenu __instance)
-            {
+        //[HarmonyPatch(typeof(XUiC_MainMenu), nameof(XUiC_MainMenu.OnOpen))]
+        //public class XUiC_MainMenu_OnOpen
+        //{
+        //    private static void Postfix(XUiC_MainMenu __instance)
+        //    {
 
-                GamePrefs.Set(EnumGamePrefs.CreativeMenuEnabled, 1);
-            }
-        }
+        //        GamePrefs.Set(EnumGamePrefs.CreativeMenuEnabled, 1);
+        //    }
+        //}
     }
 }
